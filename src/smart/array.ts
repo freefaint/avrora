aa7da9eea246f204c5380a976befe6d874d86a15
Андрей Терещенко <andrejteresenko@192.168.1.3>
2022-02-02 06:22:05 +0300

    v0.0.48

--- src/smart/array.ts
+++ src/smart/array.ts
@@ -1,7 +1,15 @@
 export const unique = <T>(arr: T[]) => {
   var collection: T[] = [];
 
-  arr.forEach(i => !collection.includes(i) && collection.push(i));
+  arr.forEach((i) => !collection.includes(i) && collection.push(i));
 
   return collection;
+};
+
+export const repeats = <T extends string>(arr: T[]) => {
+  const total: Record<string, number> = {};
+
+  arr.forEach((i) => (total[i] = total[i] !== undefined ? total[i] + 1 : 0));
+
+  return total;
 };
