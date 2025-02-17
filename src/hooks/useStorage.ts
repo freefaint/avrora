677194601c88d107988ae12e9e5f45fccff3cfa1
Андрей Терещенко <andrejteresenko@MacBook-Air-Andrej.local>
2025-02-17 17:39:28 +0300

    imporoved setStorage

--- src/hooks/useStorage.ts
+++ src/hooks/useStorage.ts
@@ -1,6 +1,6 @@
-import { useCallback, useState } from 'react';
+import { Dispatch, SetStateAction, useCallback, useState } from 'react';
 
-export const useStorage = <T>(name: string, val?: T): [T, (item: T) => void] => {
+export const useStorage = <T>(name: string, val?: T): [T, Dispatch<SetStateAction<T>>] => {
   const get = useCallback(() => {
     let val;
 
@@ -15,9 +15,17 @@ export const useStorage = <T>(name: string, val?: T): [T, (item: T) => void] => 
 
   const [value, setValue] = useState<T>(get() ?? val);
 
-  const set = useCallback((val: T) => {
-    localStorage.setItem(name, JSON.stringify(val));
-    setValue(val);
+  const set: Dispatch<SetStateAction<T>> = useCallback(val => {
+    if (val instanceof Function) {
+      setValue(old => {
+        const newVal = val(old);
+        localStorage.setItem(name, JSON.stringify(newVal));
+        return newVal;
+      });
+    } else {
+      localStorage.setItem(name, JSON.stringify(val));
+      setValue(val);
+    }
   }, []);
 
   return [value, set];
